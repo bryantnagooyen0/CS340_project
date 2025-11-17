df0f848b8f520838bd77a5cd52d269823b1d0ad2
Student <student@example.com>
2025-11-17 14:15:19 -0800

    updated column names and added left join to certain columns

--- app.py
+++ app.py
@@ -22,25 +22,36 @@ def home():
         return "An error occurred while rendering the page.", 500
 
 
-@app.route("/bsg-people", methods=["GET"])
-def bsg_people():
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+
+@app.route("/developers", methods=["GET"])
+def developers():
     try:
         dbConnection = db.connectDB()  # Open our database connection
 
         # Create and execute our queries
         # In query1, we use a JOIN clause to display the names of the homeworlds,
         #       instead of just ID values
-        query1 = "SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
-            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
-            LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;"
-        query2 = "SELECT * FROM bsg_planets;"
-        people = db.query(dbConnection, query1).fetchall()
-        homeworlds = db.query(dbConnection, query2).fetchall()
+        query1 = "SELECT developer_id AS 'Developer ID', \
+                developer_name AS 'Developer Name' \
+                FROM Developers;"
+        
+        developers = db.query(dbConnection, query1).fetchall()
+        
 
         # Render the bsg-people.j2 file, and also send the renderer
         # a couple objects that contains bsg_people and bsg_homeworld information
         return render_template(
-            "bsg-people.j2", people=people, homeworlds=homeworlds
+            "Developers.j2", developers=developers
         )
 
     except Exception as e:
@@ -52,6 +63,148 @@ def bsg_people():
         if "dbConnection" in locals() and dbConnection:
             dbConnection.close()
 
+
+@app.route("/genres", methods=["GET"])
+def genres():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT genre_id AS 'Genre ID', \
+                genre_name AS 'Genre Name' \
+                FROM Genres;"
+        
+        genres = db.query(dbConnection, query1).fetchall()
+        
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "Genres.j2", genres=genres
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+
+@app.route("/games", methods=["GET"])
+def games():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        
+        query1 = (
+            "SELECT g.game_id AS 'Game ID', "
+            "g.game_title AS 'Title', "
+            "gen.genre_name AS 'Genre', "
+            "d.developer_name AS 'Developer', "
+            "g.sales_count AS 'Sales', "
+            "g.release_date AS 'Release Date' "
+            "FROM Games g "
+            "LEFT JOIN Genres gen ON g.genre_id = gen.genre_id "
+            "LEFT JOIN Developers d ON g.developer_id = d.developer_id;"
+        )
+
+        query2 = "SELECT * FROM Developers;"
+        query3 = "SELECT * FROM Genres;"
+
+        games = db.query(dbConnection, query1).fetchall()
+        genres = db.query(dbConnection, query3).fetchall()
+        developers = db.query(dbConnection, query2).fetchall()
+        
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "Games.j2", games=games, genres=genres, developers=developers
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+
+@app.route("/reviewers", methods=["GET"])
+def reviewers():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = "SELECT reviewer_id AS 'Reviewer ID', reviewer_company AS 'Reviewer Company' FROM Reviewers;"
+
+        reviewers = db.query(dbConnection, query1).fetchall()
+        
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "Reviewers.j2", reviewers=reviewers
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
+
+@app.route("/reviews", methods=["GET"])
+def reviews():
+    try:
+        dbConnection = db.connectDB()  # Open our database connection
+
+        # Create and execute our queries
+        # In query1, we use a JOIN clause to display the names of the homeworlds,
+        #       instead of just ID values
+        query1 = (
+            "SELECT r.review_id AS 'Review ID', "
+            "g.game_title AS 'Game Title', "
+            "rev.reviewer_company AS 'Reviewer Company', "
+            "r.rating AS 'Rating', "
+            "r.comment AS 'Comment', "
+            "r.review_date AS 'Review Date' "
+            "FROM Reviews r "
+            "LEFT JOIN Reviewers rev ON r.reviewer_id = rev.reviewer_id "
+            "LEFT JOIN Games g ON r.game_id = g.game_id;"
+        )
+
+        query2 = "SELECT * FROM Reviewers;"
+        query3 = "SELECT * FROM Games;"
+        
+        reviews = db.query(dbConnection, query1).fetchall()
+        
+
+        # Render the bsg-people.j2 file, and also send the renderer
+        # a couple objects that contains bsg_people and bsg_homeworld information
+        return render_template(
+            "Reviews.j2", reviews=reviews
+        )
+
+    except Exception as e:
+        print(f"Error executing queries: {e}")
+        return "An error occurred while executing the database queries.", 500
+
+    finally:
+        # Close the DB connection, if it exists
+        if "dbConnection" in locals() and dbConnection:
+            dbConnection.close()
 
 
 # ########################################
